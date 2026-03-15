9129690afa6348d132c1cfe77f8f37ae12983622
NoahTBriggs <noahtbriggs@outlook.com>
2026-03-15 11:41:22 -0400

    Chore: Incorrectly Modified createDirs.sh

--- createDirs.sh
+++ createDirs.sh
@@ -26,8 +26,7 @@
 #         ├── bazarr
 #         │   └── config
 #         └── jellyseerr
-#             └── app
-#                 └── config
+#             └── config
 
 # NOTE: This script must be run with root privileges to ensure that the created
 #       directories have the correct ownership and permissions for the media
@@ -132,7 +131,7 @@ echo "Creating Directory Structure..."
   echo "  Creating media subdirs..." && \
   mkdir -p "$SRV_DIR/media/"{"movies","shows","music","personal_media"} && \
   echo "  Creating appdata subdirs..." && \
-  mkdir -p "$SRV_DIR/docker/appdata/"{"jellyfin/config","jellyfin/cache","sonarr/config","radarr/config","prowlarr/config","bazarr/config","jellyseerr/app/config","qbittorrent/config"}; } || \
+  mkdir -p "$SRV_DIR/docker/appdata/"{"jellyfin/config","jellyfin/cache","sonarr/config","radarr/config","prowlarr/config","bazarr/config","jellyseerr/config","qbittorrent/config"}; } || \
   { echo "Directory Creation failed."; exit 3; }
 echo "Directories Created Successfully."
 echo ""
