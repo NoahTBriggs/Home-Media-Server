bb6d5eada30ce05f4982540e2bf98e8deeab5ad5
NoahTBriggs <noahtbriggs@outlook.com>
2026-03-16 15:39:28 -0400

    Feat: Added Automatic PUID/PGID Detection

--- ID-Util.sh
+++ ID-Util.sh
@@ -13,35 +13,41 @@ function get_UID() {
 }
 
 function set_PGID() {
-  sed -i "s/^ENV_GROUP_ID=\"[0-9]*\"/ENV_GROUP_ID=\"$1\"/" .env
+  sed -i "s/^PGID=\"[0-9]*\"/PGID=\"$1\"/" .env
+}
+
+function set_jellyfin_PGID() {
+  sed -i "s/^JELLYFIN_PGID=\"[0-9]*\"/JELLYFIN_PGID=\"$1\"/" .env
 }
 
 function set_PUID() {
-  sed -i "s/^ENV_USER_ID=\"[0-9]*\"/ENV_USER_ID=\"$1\"/" .env
+  sed -i "s/^PUID=\"[0-9]*\"/PUID=\"$1\"/" .env
 }
 
 function set_IDs() {
-  local LOCAL_UID=$(get_UID);
-  local LOCAL_GID=$(get_render_GID);
+  local PUID=$(get_UID);
+  local PGID=$PUID;
+  local JELLYFIN_PGID=$(get_video_GID);
   
   # If the user's UID cannot be found use the default of 1000
-  if [ -z "$LOCAL_UID" ]; then
+  if [ -z "$PUID" ]; then
     echo "  Warning: Could not determine current user's UID. Defaulting to 1000."
-    LOCAL_UID=1000;
+    PUID=1000;
   fi
 
   # If the render group GID cannot be found, attempt to retrieve the video group GID
-  if [ -z "$LOCAL_GID" ]; then
+  if [ -z "$JELLYFIN_PGID" ]; then
     echo "  Warning: Could not determine render group GID. Attempting to retrieve video group GID..."
-    LOCAL_GID=$(get_video_GID)
+    JELLYFIN_PGID=$(get_video_GID)
   fi
 
   # If the video group GID cannot be found, use the UID
-  if [ -z "$LOCAL_GID" ]; then
+  if [ -z "$JELLYFIN_PGID" ]; then
     echo "  Warning: Could not determine video group GID. Setting PGID to match PUID..."
-    LOCAL_GID=$LOCAL_UID
+    JELLYFIN_PGID=$PUID
   fi
 
-  set_PUID "$LOCAL_UID";
-  set_PGID "$LOCAL_GID";
+  set_PUID "$PUID";
+  set_PGID "$PGID";
+  set_jellyfin_PGID "$JELLYFIN_PGID";
 }
